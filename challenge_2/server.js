933c02bf096978fac9194fe49a8b8f95b44cca4f
Ron Arbel <ronarbel1@gmail.com>
2018-12-11 12:31:43 -0800

    render text field, successful post request to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
+
+
+app.use(morgan('short'));
+app.use(express.static('./client'));
+
+app.post('/convert', (req, res) => {
+    console.log(req.body)
+  }
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
