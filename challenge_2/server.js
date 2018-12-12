311312d1bd4ceeb386479cc17b37a6fc456f50b0
Ron Arbel <ronarbel1@gmail.com>
2018-12-11 20:09:52 -0800

    upload file, return csv, allow repeat uploads

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const expressFileUpload = require('express-fileupload');
 const convertToCSV = require('./helperFunctions/convertToCSV.js').convertToCSV
 const convertToHTML = require('./helperFunctions/convertToHTML.js').convertToHTML
 const express = require('express');
@@ -12,12 +13,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(expressFileUpload());
 
 // -------- crud -------- //
 app.post('/convert', (req, res) => {
-  let CSVified = convertToCSV(req.body.input);
+  let input = req.files.input.data.toString();
+  let CSVified = convertToCSV(input);
   let HTMLified = convertToHTML(CSVified);
-  res.send(HTMLified)
+  res.send(HTMLified);
 });
 
 // -------- server -------- //
