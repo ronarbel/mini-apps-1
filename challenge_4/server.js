02985ae267b3c29a5cb3d474e22981b62982306c
Ron Arbel <ronarbel1@gmail.com>
2018-12-13 12:23:40 -0800

    Display board

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
+const morgan = require('morgan');
+
+// -------- middleware -------- //
+app.use(express.static(path.join(__dirname, 'client/dist/')));
+app.use(morgan('tiny'));
+
+
+// -------- server -------- //
+app.listen(3000, () => {console.log('Now listening on port 3000!')});
 
